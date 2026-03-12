d4c9efd9a1edbf872fc7830a498a5bc791524424
Rayyan Sameer <turbocharged.tomato@gmail.com>
2026-03-12 16:29:22 +0530

    V1 Complete

--- src/infrastructure/lambda.tf
+++ src/infrastructure/lambda.tf
@@ -16,27 +16,27 @@ resource "aws_iam_role" "lambda_role" {
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 }
 
-# DynamoDB permissions for the role
+# DynamoDB permissions
 resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
   name = "clipshare_dynamodb_access"
   role = aws_iam_role.lambda_role.id
-
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "dynamodb:PutItem",
-          "dynamodb:GetItem"
-        ]
-        Resource = aws_dynamodb_table.session_table.arn
-      }
-    ]
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["dynamodb:PutItem", "dynamodb:GetItem"]
+      Resource = aws_dynamodb_table.session_table.arn
+    }]
   })
 }
 
-# Package send.py
+# CloudWatch Logs permissions
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Package Lambdas
 data "archive_file" "send_lambda" {
   type        = "zip"
   source_file = "${path.module}/../backend/send.py"
@@ -49,69 +49,32 @@ data "archive_file" "get_lambda" {
   output_path = "${path.module}/../backend/get.zip"
 }
 
-
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
 # Send Lambda
 resource "aws_lambda_function" "send" {
-  filename         = data.archive_file.send_lambda.output_path
-  function_name    = "clipshare_send"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "send.handler"
-  runtime          = "python3.12"
-
-
+  filename      = data.archive_file.send_lambda.output_path
+  function_name = "clipshare_send"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "send.handler"
+  runtime       = "python3.12"
   environment {
     variables = {
       TABLE_NAME = aws_dynamodb_table.session_table.name
     }
   }
-
-  tags = {
-    Project = "clipshare"
-  }
+  tags = { Project = "clipshare" }
 }
 
-resource "aws_lambda_permission" "send" {
-  statement_id  = "AllowAPIGatewaySend"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.send.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.clipshare.execution_arn}/*/*"
-}
-
-resource "aws_lambda_permission" "get" {
-  statement_id  = "AllowAPIGatewayGet"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.get.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.cli}"
-}
-
- 
 # Get Lambda
 resource "aws_lambda_function" "get" {
-  filename         = data.archive_file.get_lambda.output_path
-  function_name    = "clipshare_get"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "get.handler"
-  runtime          = "python3.12"
-  
-
+  filename      = data.archive_file.get_lambda.output_path
+  function_name = "clipshare_get"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "get.handler"
+  runtime       = "python3.12"
   environment {
     variables = {
       TABLE_NAME = aws_dynamodb_table.session_table.name
     }
   }
-
-  tags = {
-    Project = "clipshare"
-  }
-
-  
-
-  
+  tags = { Project = "clipshare" }
 }
