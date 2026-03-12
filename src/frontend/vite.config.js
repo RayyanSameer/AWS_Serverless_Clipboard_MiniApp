ceb943cdd63fcb0f508d8e8a7f15db469b548568
Rayyan Sameer <turbocharged.tomato@gmail.com>
2026-03-12 14:48:05 +0530

    Fixed Testing Directories

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',  // simulates browser environment
-    globals: true         
+    globals: true  ,
+    include: ['../tests/**/*.spec.{js,jsx}']    
   }
 })
